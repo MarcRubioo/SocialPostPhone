771698f4c7ca19521328b7f49c5d41bd3c058a9c
MarcRubioo <marc.rubio@institutvidreres.cat>
2024-02-08 11:48:08 +0100

    Initial commit

---
+++ app/src/main/java/cat/insVidreres/socialpostphone/imp/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package cat.insVidreres.socialpostphone.imp.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cat.insVidreres.socialpostphone.imp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/cat/insVidreres/socialpostphone/imp/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package cat.insVidreres.socialpostphone.imp.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cat.insVidreres.socialpostphone.imp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
